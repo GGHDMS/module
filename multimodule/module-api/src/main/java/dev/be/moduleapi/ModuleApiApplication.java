f3aa34001141e7013c764e46e96d62046fefcf18
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-05-07 20:08:33 +0900

    Multi Module DB 연동

--- multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -2,10 +2,14 @@ package dev.be.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
         scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}
 )
+@EntityScan("dev.be.modulecommon.domain") // entity 를 어디서 찾을지
+@EnableJpaRepositories(basePackages = {"dev.be.modulecommon.repository"}) // 레포지토리를 어디서 찾을지
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
--- multimodule/module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
+++ multimodule/module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -1,7 +1,6 @@
 package dev.be.moduleapi.controller;
 
 import dev.be.moduleapi.service.DemoService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
